330fb82c78030db8ae57c72071023fafb2353a9d
Sagnik Saha <sagnik12347@gmail.com>
2025-09-29 10:55:41 +0530

    Initial Commit

---
+++ Apple Intelligence App/Apple_Intelligence_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Intelligence_AppApp.swift
+//  Apple Intelligence App
+//
+//  Created by Sagnik Saha on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Intelligence_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Intelligence App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple Intelligence App
+//
+//  Created by Sagnik Saha on 29/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
